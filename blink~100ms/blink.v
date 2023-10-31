1173da25101d26911888e19dd829f32fd9494e34
Joe-Pacifiker <145924934+Joe-Pacifiker@users.noreply.github.com>
2023-10-30 18:13:27 -0700

    blink at roughly 100ms design files
    The counter needs to be checking just about the 20th MSB since its counter is a factor of 10 less than the 1 second delay. Log base 2 of 10 is closest to the whole number 3. So 1 second~2^23 divided by 10~2^3 is ~2^20.

---
+++ blink~100ms/blink.v
@@ -0,0 +1,25 @@
+
+module switch(  input CLK,
+                output LED
+                );
+      
+   reg [25:0] counter;
+   // According to the Makefile we use 48MHz clock
+   // However, this FPGA does not have 48MHz clock
+   // It does however have a 36MHz clock...
+   //  So counter bit 25 should be closer to 1 hz freq
+   assign LED = ~counter[20]; //1001100110011001100110100
+   //assign LED = ~counter[21];
+
+   initial begin
+      counter = 0;
+   end
+
+   always @(posedge CLK)
+   begin
+      counter <= counter + 1;
+   end
+
+
+
+endmodule
