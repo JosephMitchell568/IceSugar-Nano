7ccf8d274642a0d03a70059b93d3d868d00ea49c
Joe-Pacifiker <145924934+Joe-Pacifiker@users.noreply.github.com>
2024-07-13 14:41:13 -0700

    Fully implemented with comments
    I say 'fully' implemented since I need to create a top level module in order to add all necessary inputs and outputs.
    All information necessary for the instruction fetch stage is described in comments including the PCsrc control bit and branchJmpAddr from Memory stage.
    I will incorporate this stage with a top level module soon in order to emulate it with the IceSugar-Nano FPGA.
    My next step is to write a script to generate random R-Type instructions and fill exactly 1 BRAM worth of Instruction Memory.

--- mips/fetch/fetch.v
+++ mips/fetch/fetch.v
@@ -5,24 +5,35 @@
 
 module fetch(
  input CLK
+ //Later there must be a PCsrc control bit added
+ // and a 32 bit branch/jump address supplied as inputs
+ //Later there must be an IF/ID Pipe as output which includes
+ // a 32 bit instruction word
+ // a 32 bit pc address for next instruction
  );
 
  reg [31:0] instruction; //Instruction fetched from instMemory
  reg [31:0] pc; //Program Counter (Points to next instruction)
 
+ //-------------- InstructionMem1 inputs ---------------------
  reg instRdEn1; // Read enable line
  reg [6:0] instRdAddr1; // 7 bit - 128 addresses
-
+ //-------------- End InstructionMem1 inputs -----------------
+ 
+ //-------------- InstructionMem1 outputs --------------------
  wire [31:0] instDataOut1;
  wire instValidOut1;
+ //-------------- End InstructionMem1 outputs ----------------
 
+ //-------------- InstructionMem1 module instantiaton --------
  instructionMem1 instructionMem1Inst(
   .clk(CLK),
   .rd_en(instRdEn1),
   .rd_addr(instRdAddr1),
   .data_out(instDataOut1),
   .valid_out(instValidOut1)
-  );
+ );
+ //-------------- End instructionMem1 module Instantation ----
 
  initial
  begin
@@ -34,7 +45,16 @@ module fetch(
 
  always @(posedge CLK)
  begin
-   
+  //if(PCsrc)
+  //begin
+  // pc[31:0] <= brchJmpAddr;
+  //end
+  //else
+  //begin
+  pc[31:0] <= pc[31:0] + 32'd4; // Increment PC by 4 bytes
+  //end
+  instruction[31:0] <= instDataOut1;
+  instRdAddr1[6:0] <= pc[6:2]; //7 bit byte addressable (word)
  end
  
 endmodule
